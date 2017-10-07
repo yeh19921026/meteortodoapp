8f88412e963df6aa8f3285e115fd8b10c14ccc59
MingMing <yeh19921026@gmail.com>
2017-10-07 18:35:12 +0800

    step2

---
+++ import/ui/body.js
@@ -0,0 +1,12 @@
+import { Template } from 'meteor/templating';
+
+import '/body.html';
+
+Template.body.helpers({
+
+    task:[
+        {text:'task 1'},
+        {text:'task 2'},
+        {text:'task 3'},
+    ],
+});
